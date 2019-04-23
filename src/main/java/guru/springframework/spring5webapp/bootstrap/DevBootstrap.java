66b66074596550c4bc2330eb017bc2579305a2db
Rebecca Luttmer <rebecca.luttmer@gmail.com>
2019-04-22 20:32:31 -0500

    Added class Publisher, added one to one relationship, changes book and dev bootstrap from string to publisher object

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,6 +2,7 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
 import org.springframework.context.ApplicationListener;
@@ -31,7 +32,8 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Eric
         Author eric = new Author("Eric", "Evans");
-        Book  ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Publisher hc = new Publisher("Harper Collins", "Harper Collins Drive");
+        Book  ddd = new Book("Domain Driven Design", "1234", hc);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -41,7 +43,8 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Rod
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Wrox" );
+        Publisher wr = new Publisher("Harper Collins", "Harper Collins Drive");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", wr );
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
